19c64fc16cb088e5946a2e2b6a1e49c61badd2b0
Hiesenberg123 <34301870+Hiesenberg123@users.noreply.github.com>
2021-09-12 02:02:40 +0530

    Update app.R

--- app.R
+++ app.R
@@ -3,6 +3,6 @@ library(shiny.semantic)
  
 ships <- readr::read_csv(unzip("ships_04112020.zip", "ships.csv"))
 ships$Time <- format(ships$DATETIME,"%H:%M:%S")
-
+ships<- ships[c(1:1000000),]
 shinyAppDir("app")
 
